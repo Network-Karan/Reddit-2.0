fd9d40a4900b9e6a134c9bfe5f8cc1464f496253
Karan <kaliyaperumal.diwakaran@gmail.com>
2022-05-19 16:00:03 +0530

    init

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,62 @@
 import React from 'react'
 import Image from 'next/image'
+import {
+    BellIcon,
+    ChatIcon,
+    GlobeIcon,
+    PlusIcon,
+    SparklesIcon,
+    SpeakerphoneIcon,
+    VideoCameraIcon,
+} from '@heroicons/react/outline'
+
+import { 
+    ChevronDownIcon, 
+    HomeIcon, 
+    MenuIcon,
+    SearchIcon,
+ } from '@heroicons/react/solid'
+
+
+
 
 function Header() {
   return (
-    <div>
-        <div className="relative h-10 w-20">
+    <div className= "sticky tpo-0  z-50 flex bg-white px-4 py-2 shadow-sm">
+        <div className="relative h-10 w-20 flex-shrink-0 cursor-pointer">
             <Image
              objectFit="contain"
              src="https://links.papareact.com/fqy" 
              layout="fill"
             />
+        </div>
+
+        <div className="mx-7 flex items-center xl:min:min-w-[300px]">
+            <HomeIcon className="h-5 w-5"/>
+            <p className="ml-2 hidden flex-1 lg:inline">Home</p>
+            <ChevronDownIcon className="h-5 w-5" />
+        </div>
+
+        {/*Search box*/}
+        <form className="flex flex-1 items-center space-x-2 rounded-sm border border-gray-200 bg-gray-100 px-3 py-1">
+            <SearchIcon className="h-6 w-6 text-gray-400" />
+            <input 
+            className="flex-1 bg-transparent outline-none"
+            type="text" 
+            placeholder="Search Reddit" />
+            <button type="submit" hidden />
+        </form>
+        
+
+        <div className="flex">
+            <SparklesIcon className="icon" />
+            <GlobeIcon className="icon" />
+            <VideoCameraIcon className="icon" />
+            <hr className="hr-10 border border-gray-100" />
+            <ChatIcon className="icon" />
+            <BellIcon className="icon" />
+            <PlusIcon className="icon" />
+            <SpeakerphoneIcon className="icon" />
         </div>
     </div>
   )
